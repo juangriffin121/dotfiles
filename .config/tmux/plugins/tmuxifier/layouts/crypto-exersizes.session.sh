2349948326e93bd803e2400eabe02f53dce3d2cb
griffin <juanogriffin@gmail.com>
2026-07-23 12:45:19 -0300

    small changes

---
+++ .config/tmux/plugins/tmuxifier/layouts/crypto-exersizes.session.sh
@@ -0,0 +1,22 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "/mnt/win_share/Users/Usuario/my_scripts/crypto-exercises/"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "crypto-exersizes"; then
+  run_cmd "poetry shell"
+  new_window "code"
+  split_h 50
+
+  select_pane 1
+  run_cmd "nvim"
+
+  new_window "ai"
+
+  select_window 1
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
