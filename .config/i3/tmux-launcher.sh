0949509702d014b7c4881217fca971b1d75425fd
griffin <juanogriffin@gmail.com>
2026-09-22 20:39:33 -0300

    Switched alacritty for kitty

--- .config/i3/tmux-launcher.sh
+++ .config/i3/tmux-launcher.sh
@@ -5,9 +5,9 @@ selected=$(echo -e "$(tmuxifier list-sessions)" | custom_menu1 -i -p "Open tmux 
 if [ -n "$selected" ]; then
     option=$(echo -e "Load\nEdit"| custom_menu1 -i -p "Open tmux session:") # custom_menu1 is a rofi script
     case "$option" in
-      "Edit") alacritty -e bash -ic "tmuxifier edit-session $selected"
+      "Edit") kitty -e bash -ic "tmuxifier edit-session $selected"
       ;;
-      "Load") alacritty -e bash -ic "tmuxifier load-session $selected"
+      "Load") kitty -e bash -ic "tmuxifier load-session $selected"
       ;;
       *) echo "Something wong"
       ;;
